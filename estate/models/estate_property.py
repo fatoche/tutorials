4568d556bf82def1e967c01d3dddddd8a707c199
Clara Scherer <clara.scherer@inovex.de>
2026-07-10 15:12:38 +0200

    Add fields to db model

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -6,3 +6,18 @@ class EstateProperty(models.Model):
     _description = "Estate Property"
 
     name = fields.Char("Property name")
+    description = fields.Text("Description")
+    postcode = fields.Char("Postcode")
+    date_availability = fields.Date("Availability Date")
+    expected_price = fields.Float("Expected Price")
+    selling_price = fields.Float("Selling Price")
+    bedrooms = fields.Integer("Number of Bedrooms")
+    living_area = fields.Integer("Living Area")
+    facades = fields.Integer("Facades")
+    garage = fields.Boolean("Garage")
+    garden = fields.Boolean("Garden")
+    garden_area = fields.Integer("Garden Area")
+    garden_orientation = fields.Selection(
+        string='Garden Orientation',
+        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
+    )

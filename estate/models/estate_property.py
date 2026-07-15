c5ea4a0e5da369fd4001882c5713c86924f48d5a
Clara Scherer <clara.scherer@inovex.de>
2026-07-15 10:08:55 +0200

    Add tags to property

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -29,6 +29,9 @@ class EstateProperty(models.Model):
         required=True,
         copy=False,
     )
+
     property_type_id = fields.Many2one("estate.property.type", string="Property Type")
     buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
     salesperson_id = fields.Many2one("res.users", string="Salesperson", default=lambda self: self.env.user.id)
+
+    tag_ids = fields.Many2many("estate.property.tag", string="Property Tags")

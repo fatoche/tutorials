03b408b22e02fea69b184bb13a9549a7d622c9bb
Clara Scherer <clara.scherer@inovex.de>
2026-07-16 13:04:52 +0200

    Add computed field with inverse

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
@@ -8,3 +8,19 @@ class EstatePropertyOffer(models.Model):
     status = fields.Selection(string="Status", selection=[("accepted", "Accepted"), ("refused", "Refused")])
     partner_id = fields.Many2one("res.partner", string="Partner", required=True)
     property_id = fields.Many2one("estate.property", required=True)
+
+    validity = fields.Integer("Validity (days)", default=7)
+    date_deadline = fields.Date("Deadline", compute="_compute_deadline", inverse="_inverse_deadline")
+
+
+    @api.depends("create_date", "validity")
+    def _compute_deadline(self):
+        for record in self:
+            create_date = record.create_date.date() if record.create_date else fields.Date.today()
+            record.date_deadline = fields.Date.add(create_date, days=record.validity)
+
+    
+    def _inverse_deadline(self):
+        for record in self:
+            create_date = record.create_date.date() if record.create_date else fields.Date.today()
+            record.validity = (record.date_deadline - create_date).days

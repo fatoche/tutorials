ad52b13107727345b97792b5ee307294c24a9fd9
Clara Scherer <clara.scherer@inovex.de>
2026-07-16 14:25:29 +0200

    Set buyer and selling price on accepting an offer

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -29,6 +29,8 @@ class EstatePropertyOffer(models.Model):
     def action_accept(self):
         for record in self:
             record.status = "accepted"
+            record.property_id.buyer_id = record.partner_id
+            record.property_id.selling_price = record.price
 
 
     def action_refuse(self):

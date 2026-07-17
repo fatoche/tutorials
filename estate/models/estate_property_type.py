89b032b2ce25fd6b860ee5b5e30284ed3a542d00
Clara Scherer <clara.scherer@inovex.de>
2026-07-17 14:07:01 +0200

    Add back reference from property type to property

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -5,6 +5,7 @@ class EstatePropertyType(models.Model):
     _description = "Estate Property Type"
 
     name = fields.Char("Typname", required=True)
+    property_ids = fields.One2many("estate.property", "property_type_id", string="Properties")
 
     _sql_constraints = [
         ("unique_name", "UNIQUE(name)", "Name must be unique.")

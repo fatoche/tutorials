b23738e08d957ca7648989d092e0d21f23f86563
Clara Scherer <clara.scherer@inovex.de>
2026-07-17 15:13:15 +0200

    Add tag colors

--- estate/models/estate_property_tag.py
+++ estate/models/estate_property_tag.py
@@ -6,6 +6,7 @@ class EstatePropertyTag(models.Model):
     _order = "name"
 
     name = fields.Char("Name", required=True)
+    color = fields.Integer("Color")
 
     _sql_constraints = [
         ("unique_name", "UNIQUE(name)", "Name must be unique.")

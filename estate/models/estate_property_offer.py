d9cc77bb5eacdeb4b4b3795771301a23bd1a6970
Clara Scherer <clara.scherer@inovex.de>
2026-07-21 09:14:05 +0200

    Add stat button

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -9,6 +9,7 @@ class EstatePropertyOffer(models.Model):
     status = fields.Selection(string="Status", selection=[("accepted", "Accepted"), ("refused", "Refused")])
     partner_id = fields.Many2one("res.partner", string="Partner", required=True)
     property_id = fields.Many2one("estate.property", required=True)
+    property_type_id = fields.Many2one(related="property_id.property_type_id")
 
     validity = fields.Integer("Validity (days)", default=7)
     date_deadline = fields.Date("Deadline", compute="_compute_deadline", inverse="_inverse_deadline")
--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
@@ -9,6 +9,14 @@ class EstatePropertyType(models.Model):
     sequence = fields.Integer("Sequence", default=1)
     property_ids = fields.One2many("estate.property", "property_type_id", string="Properties")
 
+    offer_ids = fields.One2many("estate.property.offer", "property_type_id", string="Offers")
+    offer_count = fields.Integer(compute="_compute_offer_count")
+
     _sql_constraints = [
         ("unique_name", "UNIQUE(name)", "Name must be unique.")
     ]
+
+    @api.depends("offer_ids")
+    def _compute_offer_count(self):
+        for record in self:
+            record.offer_count = len(record.offer_ids)
